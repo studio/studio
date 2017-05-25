4246a2c4cf4c6154077f906126d719556ec800ba
Luke Gorrie <luke@snabb.co>
2017-05-25 09:08:33 +0000

    pkgs.nix: Bump to latest nixpkgs
    This is mostly to get the post-1.0 rstudio release.

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/6a0155d2b7cb10aef1c63b654a2b172d78fd89b4.tar.gz)
+import (fetchTarball https://github.com/NixOS/nixpkgs/archive/760248360c1d3b2a0a648a76803180e7a6c13275.tar.gz)
