9a4d9ff2b382ff602488d7ad49bdd4b61596e755
Luke Gorrie <luke@snabb.co>
2017-07-27 10:31:49 +0000

    overlays.nix: Studio packages are now top-level

--- overlays.nix
+++ overlays.nix
@@ -5,14 +5,12 @@
   (self: super:
     with super.callPackage ./backend/frontend {};
     {
-      studio = {
-        inherit studio-gui studio-gui-vnc studio-base-image studio-image;
-        raptorjit = super.callPackage ./backend/raptorjit {};
-        snabbr = super.callPackage ./backend/snabbr {};
-        timeliner = super.callPackage ./backend/timeliner {};
-        vmprofiler = super.callPackage ./backend/vmprofiler {};
-        dwarfish = super.callPackage ./backend/dwarfish {};
-      };
+      inherit studio-gui studio-gui-vnc studio-base-image studio-image;
+      raptorjit = super.callPackage ./backend/raptorjit {};
+      snabbr = super.callPackage ./backend/snabbr {};
+      timeliner = super.callPackage ./backend/timeliner {};
+      vmprofiler = super.callPackage ./backend/vmprofiler {};
+      dwarfish = super.callPackage ./backend/dwarfish {};
     })
 ]
 
