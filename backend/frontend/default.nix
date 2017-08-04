c0f598c2a8faabbab2282923d1e0aa3c16b07247
Luke Gorrie <luke@snabb.co>
2017-08-04 09:55:18 +0000

    Installation made simpler
    Now you install with 'nix-env -iA studio' and that automatically
    installs the x11 version, the vnc version, and tigervnc for easy
    access to 'vncserver -kill ...'.
    
    So installation is always done in the same way and you choose between
    X11 vs VNC when you start Studio.

--- backend/frontend/default.nix
+++ backend/frontend/default.nix
@@ -1,6 +1,8 @@
 { pkgs }:
 with pkgs; with stdenv;
 
+let studio-version = import ../../nix/studio-version.nix; in
+
 let
   # Function to fetch a Pharo image from a zip file.
   fetchImageZip = { name, version, url, sha256 }:
@@ -20,8 +22,8 @@ let
 
   # Pharo image that includes all external dependencies.
   # Built on Inria CI (Jenkins) with Metacello to install Studio.
-  baseImage = fetchImageZip rec {
-    name = "studio-${version}";
+  base-image = fetchImageZip rec {
+    name = "studio-base-image-${version}";
     version = "19";
     url = "https://ci.inria.fr/pharo-contribution/job/Studio/default/${version}/artifact/Studio.zip";
     sha256 = "19jhw0fnca4450a7iv51mzq91wixm5gllq7qwnw9r4yxhnkm3vak";
@@ -49,10 +51,10 @@ let
 
   # Studio image that includes the exact code in this source tree.
   # Built by refreshing the base image.
-  studioImage = runCommand "studio-image"
+  studio-image = runCommand "studio-image"
     { nativeBuildInputs = [ pharo ]; }
     ''
-      cp ${baseImage}/* .
+      cp ${base-image}/* .
       chmod +w pharo.image
       chmod +w pharo.changes
       pharo --nodisplay pharo.image st --quit ${loadSmalltalkScript}
@@ -62,49 +64,62 @@ let
     '';
 
   # Script to start the Studio image with the Pharo VM.
-  studioScript = writeScript "studio-gui" ''
-    #!${stdenv.shell}
-    cp ${studioImage}/pharo.image pharo.image
-    cp ${studioImage}/pharo.changes pharo.changes
-    chmod +w pharo.image
-    chmod +w pharo.changes
-    export STUDIO_PATH=''${STUDIO_PATH:-${../..}}
-    ${pharo}/bin/pharo pharo.image
-  '';
+  studio-x11 = writeScriptBin "studio-x11-${studio-version}"
+    ''
+      #!${stdenv.shell}
+      cp ${studio-image}/pharo.image pharo.image
+      cp ${studio-image}/pharo.changes pharo.changes
+      chmod +w pharo.image
+      chmod +w pharo.changes
+      export STUDIO_PATH=''${STUDIO_PATH:-${../..}}
+      ${pharo}/bin/pharo pharo.image
+    '';
 
   # Configuration file to make ratpoison run Studio.
-  ratpoisonConfig = writeScript "studio-ratpoison-config" ''
-    escape F1
-    exec ${studioScript}
-  '';
+  ratpoisonConfig = writeScript "studio-ratpoison-config"
+    ''
+      escape F1
+      exec ${studio-x11}/bin/studio-x11
+    '';
 
   # Script to run ratpoison with the config for Studio.
-  ratpoisonScript = writeScript "studio-ratpoison" ''
-    #!${stdenv.shell}
-    exec ${ratpoison}/bin/ratpoison -f ${ratpoisonConfig}
-  '';
+  ratpoisonScript = writeScript "studio-ratpoison"
+    ''
+      #!${stdenv.shell}
+      exec ${ratpoison}/bin/ratpoison -f ${ratpoisonConfig}
+    '';
 
   # Script to run everything in VNC.
-  vncScript = writeScriptBin "studio-gui-vnc" ''
+  studio-vnc = writeScriptBin "studio-vnc-${studio-version}" ''
     #!${stdenv.shell}
-    if [ $# == 0 ]; then
-      display=1
-    elif [ $# == 1 ]; then
-      display=$1
-    fi
-    exec ${tigervnc}/bin/vncserver :$display \
-      -name "Studio (:$display)" \
-      -fg \
+    exec ${tigervnc}/bin/vncserver \
+      "$@" \
+      -name "Studio" \
       -autokill \
       -xstartup ${ratpoisonScript} \
       -SecurityTypes None
   '';
+
+  studio = runCommand "studio-${studio-version}"
+    {
+      meta = {
+        description = "A productive environment for working on your programs";
+      };
+    }
+    ''
+      mkdir -p $out/bin
+      ln -s ${studio-x11}/bin/studio-x11 $out/bin
+      ln -s ${studio-vnc}/bin/studio-vnc $out/bin
+    '';
 in
   
 {
-  studio-gui = studioScript;
-  studio-gui-vnc = vncScript;
-  studio-base-image = baseImage;
-  studio-image = studioImage;
+  # main package collection for 'nix-env -i'
+  inherit studio;
+  # individual packages
+  studio-gui = studio-x11;           # deprecated
+  studio-gui-vnc = studio-vnc;       # deprecated
+  studio-base-image = base-image;
+  studio-image = studio-image;
 }
 
--- backend/inspector/default.nix
+++ backend/inspector/default.nix
@@ -3,6 +3,7 @@
 with pkgs;
 
 {
+  raptorjit-auditlog = modules/raptorjit-auditlog.nix args;
   hexdump = import modules/hexdump.nix args;
   pdl     = import modules/pcap2xml.nix args;
   vmprofile = import modules/vmprofile.nix args;
