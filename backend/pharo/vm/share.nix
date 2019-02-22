02c011d3a3410ef72e8e349cb3acee7d246d34bd
Luke Gorrie <luke@snabb.co>
2019-02-22 15:11:35 +0000

    Fix pharo sources (hopefully)

--- backend/pharo/vm/share.nix
+++ backend/pharo/vm/share.nix
@@ -1,17 +1,8 @@
 { stdenv, fetchurl, unzip, runCommand }:
 
-runCommand "pharo-share-1.0"
-  rec {
-    sources60Zip = fetchurl {
-      url = http://files.pharo.org/sources/PharoV60.sources.zip;
-      sha256 = "0xbdi679ryb2zg412xy6zkh22l20pmbl92m3qhfgzjvgybna8z2a";
-    };
-
-    buildInputs = [ unzip ];
-  }
+runCommand "pharo-share-1.0" { buildInputs = [ unzip ]; }
   ''
     mkdir -p $out/lib
-    unzip $sources60Zip -d $out/lib/
-    cp ${../../frontend/Pharo7.0-32bit-14a515b.sources} $out/lib/
-    cp ${../../frontend/Pharo7.0-32bit-362a6cd.sources} $out/lib/
+    cp ${./sources}/Pharo7.0-32bit-14a515b.sources $out/lib/
+    cp ${./sources}/Pharo7.0-32bit-362a6cd.sources} $out/lib/
   ''
