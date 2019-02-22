3945a4ac3d70b8c7b89cd3db161471a4ef3a54a0
Luke Gorrie <luke@snabb.co>
2019-02-22 15:14:49 +0000

    fixup! Fix pharo sources (hopefully)

--- backend/pharo/vm/share.nix
+++ backend/pharo/vm/share.nix
@@ -3,6 +3,5 @@
 runCommand "pharo-share-1.0" { buildInputs = [ unzip ]; }
   ''
     mkdir -p $out/lib
-    cp ${./sources}/Pharo7.0-32bit-14a515b.sources $out/lib/
-    cp ${./sources}/Pharo7.0-32bit-362a6cd.sources} $out/lib/
+    cp ${./sources}/*.sources $out/lib/
   ''
